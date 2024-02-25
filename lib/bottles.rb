26aaf171c4ca54b7d81e71d4a1f8bc66669f5c4c
Rubén S. García <rssanjuan2704@gmail.com>
2024-02-25 22:18:36 +0100

    flocking rules applied

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -14,16 +14,19 @@ class Bottles
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    when 1
-      "#{number} #{container(number)} of beer on the wall, " +
-      "#{number} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
+      "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
     end
   end
 
