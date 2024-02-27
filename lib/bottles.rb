9685514398bc239c68d150f9b61b3d88df332928
Rubén S. García <rssanjuan2704@gmail.com>
2024-02-27 22:44:46 +0100

    refactor: Extract BottleNumber class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,42 +15,70 @@ class Bottles
   end
 
   def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    BottleNumber.new(number).successor
   end
 
   def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+    BottleNumber.new(number).action
   end
 
   def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+    BottleNumber.new(number).quantity
   end
 
   def container(number)
-    if number == 1
+    BottleNumber.new(number).container
+  end
+
+  def pronoun(number)
+    BottleNumber.new(number).pronoun
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
+  def successor
+    if @number == 0
+      99
+    else
+      @number - 1
+    end
+  end
+
+  def action
+    if @number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def quantity
+    if @number == 0
+      "no more"
+    else
+      @number.to_s
+    end
+  end
+
+  def container
+    if @number == 1
       "bottle"
     else
       "bottles"
     end
   end
 
-  def pronoun(number)
-    if number == 1
+  def pronoun
+    if @number == 1
       "it"
     else
       "one"
     end
   end
+
 end
