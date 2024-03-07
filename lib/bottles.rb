cbe49b9c30e648b12cc39a59fe40579a2546ba39
Rubén S. García <rssanjuan2704@gmail.com>
2024-03-07 21:26:32 +0100

    everything works

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,14 +8,17 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number      = BottleNumber.for(number)
+    BottleVerse.new(number).verse(number)
 
-    "#{bottle_number} of beer on the wall, ".capitalize + 
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
+    # bottle_number      = BottleNumber.for(number)
+
+    # "#{bottle_number} of beer on the wall, ".capitalize + 
+    # "#{bottle_number} of beer.\n" +
+    # "#{bottle_number.action}, " +
+    # "#{bottle_number.successor} of beer on the wall.\n"
   end
-end
+
+  end
 
 class BottleVerse
   attr_reader :number
@@ -23,6 +26,16 @@ class BottleVerse
   def initialize(number)
     @number = number
   end
+
+  def verse(number)
+    bottle_number      = BottleNumber.for(number)
+
+    "#{bottle_number} of beer on the wall, ".capitalize + 
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{bottle_number.successor} of beer on the wall.\n"
+  end
+
 end
 
 class BottleNumber
