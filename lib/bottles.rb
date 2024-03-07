d38293b5c46d7f018a246c5cb2b6dce87fa288e1
Rubén S. García <rssanjuan2704@gmail.com>
2024-03-07 19:28:34 +0100

    Move creation logic into classes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,11 +19,12 @@ end
 
 class BottleNumber
   def self.for(number)
-    Hash.new(BottleNumber).merge(
-      0 => BottleNumber0,
-      1 => BottleNumber1,
-      6 => BottleNumber6
-    )[number].new(number)
+    [BottleNumber0, BottleNumber1, BottleNumber6, BottleNumber].
+      find {|candidate| candidate.handles?(number)}.new(number)
+  end
+
+  def self.handles?(number)
+    true
   end
 
   attr_reader :number
@@ -57,6 +58,10 @@ class BottleNumber
 end
 
 class BottleNumber0 < BottleNumber
+  def self.handles?(number)
+    number == 0
+  end
+
   def quantity
     "no more"
   end
@@ -71,6 +76,10 @@ class BottleNumber0 < BottleNumber
 end
 
 class BottleNumber1 < BottleNumber
+  def self.handles?(number)
+    number == 1
+  end
+
   def container
     "bottle"
   end
@@ -81,6 +90,10 @@ class BottleNumber1 < BottleNumber
 end
 
 class BottleNumber6 < BottleNumber
+  def self.handles?(number)
+    number == 6
+  end
+
   def container
     "six-pack"
   end
