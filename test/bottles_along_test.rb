65439c5a4704ae9d7624315a32fdcdb709789739
Rubén S. García <rssanjuan2704@gmail.com>
2024-03-11 22:46:11 +0100

    exercise BottleNumber hierarchy

--- test/bottles_along_test.rb
+++ test/bottles_along_test.rb
@@ -385,4 +385,15 @@ class BottlesTest < Minitest::Test
     SONG
     assert_equal expected, Bottles.new.song
   end
+
+  def test_returns_correct_class_for_given_number
+    assert_equal BottleNumber0, BottleNumber.for(0).class
+    assert_equal BottleNumber1, BottleNumber.for(1).class
+    assert_equal BottleNumber6, BottleNumber.for(6).class
+
+
+    assert_equal BottleNumber, BottleNumber.for(2).class
+    assert_equal BottleNumber, BottleNumber.for(3).class
+    assert_equal BottleNumber, BottleNumber.for(99).class
+  end
 end
