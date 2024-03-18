ee5ceba9b993434398e7c02afa1288aab1bb0b2f
Rubén S. García <rssanjuan2704@gmail.com>
2024-03-18 23:35:36 +0100

    complete BottleVerse tests

--- test/bottles_along_test.rb
+++ test/bottles_along_test.rb
@@ -354,7 +354,7 @@ class BottlesTest < Minitest::Test
 end
 
 class BottleVerseTest < Minitest::Test
-  def test_the_first_verse
+  def test_verse_general_rule_upper_bound
     expected = 
       "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
@@ -363,13 +363,31 @@ class BottleVerseTest < Minitest::Test
     assert_equal expected, BottleVerse.lyrics(99)
   end
 
-  def test_another_verse
+  def test_verse_general_rule_lower_bound
     expected =
       "3 bottles of beer on the wall, " +
       "3 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "2 bottles of beer on the wall.\n"
     assert_equal expected, BottleVerse.lyrics(3)
+  end
+
+  def test_verse_7
+      expected = "7 bottles of beer on the wall, " +
+      "7 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of beer on the wall.\n"
+
+      assert_equal expected, BottleVerse.lyrics(7)
+  end
+
+  def test_verse_6
+      expected = "1 six-pack of beer on the wall, " +
+      "1 six-pack of beer.\n" +
+      "Take one down and pass it around, " +
+      "5 bottles of beer on the wall.\n"
+
+      assert_equal expected, BottleVerse.lyrics(6)
   end
 
   def test_verse_2
